6ad86a1276ff2d1ccce5657c3be3d0212421fbb0
Javier Gil <javiergilmont@gmail.com>
2023-10-19 15:59:37 +0200

    feat: noqa on magic value comparation

--- tests/beginner/classes_and_objects/test_classes_and_objects.py
+++ tests/beginner/classes_and_objects/test_classes_and_objects.py
@@ -12,35 +12,35 @@ def test_pizza() -> None:
         ],
     )
     assert isinstance(pizza, Pizza) is True
-    assert len(pizza.ingredients) == 5
-    assert pizza.price() == 15
+    assert len(pizza.ingredients) == 5  # noqa: PLR2004
+    assert pizza.price() == 15  # noqa: PLR2004
 
 
 def test_pizza_napolitana() -> None:
     pizza_napolitana: Pizza = Pizza.napolitana()
     assert isinstance(pizza_napolitana, Pizza) is True
-    assert len(pizza_napolitana.ingredients) == 3
-    assert pizza_napolitana.price() == 9
+    assert len(pizza_napolitana.ingredients) == 3  # noqa: PLR2004
+    assert pizza_napolitana.price() == 9  # noqa: PLR2004
 
 
 def test_pizza_four_cheese() -> None:
     pizza_four_cheese: Pizza = Pizza.four_cheese()
     assert isinstance(pizza_four_cheese, Pizza) is True
-    assert len(pizza_four_cheese.ingredients) == 4
-    assert pizza_four_cheese.price() == 12
+    assert len(pizza_four_cheese.ingredients) == 4  # noqa: PLR2004
+    assert pizza_four_cheese.price() == 12  # noqa: PLR2004
 
 
 def test_change_price_per_ingredient() -> None:
     pizza_napolitana: Pizza = Pizza.napolitana()
-    assert pizza_napolitana.price() == 9
+    assert pizza_napolitana.price() == 9  # noqa: PLR2004
 
     pizza_four_cheese: Pizza = Pizza.four_cheese()
-    assert pizza_four_cheese.price() == 12
+    assert pizza_four_cheese.price() == 12  # noqa: PLR2004
 
     # updating a class attribute, AFFECTS ALL INSTANCES!
     Pizza.price_per_ingredient = 3.5
-    assert pizza_napolitana.price() == 10.5
-    assert pizza_four_cheese.price() == 14.0
+    assert pizza_napolitana.price() == 10.5  # noqa: PLR2004
+    assert pizza_four_cheese.price() == 14.0  # noqa: PLR2004
 
     # updating class attribute to original value
     Pizza.price_per_ingredient = 3
