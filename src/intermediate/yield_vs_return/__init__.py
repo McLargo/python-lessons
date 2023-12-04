ac496749b4db817972456a729c78ca28dba81f40
Javier <690606+McLargo@users.noreply.github.com>
2023-12-04 11:35:31 +0100

    Feat: yield vs return (#14)
    * feat: new lesson yield vs return

---
+++ src/intermediate/yield_vs_return/__init__.py
@@ -0,0 +1,12 @@
+"""Module to compare yield vs return.
+
+This module contains the functions to compare the use of yield vs return.
+
+"""
+
+
+from .yield_vs_return import (
+    return_even_numbers,  # noqa: F401
+    yield_even_numbers,  # noqa: F401
+    yield_fibonacci_numbers,  # noqa: F401
+)
---
+++ src/intermediate/yield_vs_return/yield_vs_return.py
@@ -0,0 +1,48 @@
+"""Module to compare yield vs return.
+
+This module contains the functions to compare the use of yield vs return.
+
+"""
+
+
+from typing import Generator
+
+
+def return_even_numbers(n: int) -> list[int]:
+    """Return even numbers until n (inclusive).
+
+    Returns:
+        even_numbers: List of even numbers until n (inclusive).
+    """
+    numbers: list[int] = []
+    for number in range(1, n):
+        if number % 2 == 0:
+            numbers.append(number)
+    return numbers
+
+
+def yield_even_numbers(n: int) -> Generator:
+    """Yield even numbers until n (inclusive).
+
+    Returns:
+        even_numbers: List of even numbers until n (inclusive).
+    """
+    for number in range(1, n):
+        if number % 2 == 0:
+            yield number
+
+
+def yield_fibonacci_numbers() -> Generator:
+    """Yield Fibonacci series.
+
+    Returns:
+        fibonacci_numbers: List of numbers of the Fibonacci series.
+    """
+    c1, c2 = 0, 1
+    count = 0
+    while True:
+        yield c1
+        c3 = c1 + c2
+        c1 = c2
+        c2 = c3
+        count += 1
