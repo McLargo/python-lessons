642d20a852735d9196f49f7ba7f6b2ddf9e61be8
Javier <690606+McLargo@users.noreply.github.com>
2024-02-20 12:40:11 +0100

    Fix make build error (#22)
    Add new pre-commit hook to execute make build,
    so in case there is an error, you can fix before commiting.

--- src/advanced/lambda_functions/__init__.py
+++ src/advanced/lambda_functions/__init__.py
@@ -3,3 +3,8 @@
 This module contains examples about lambda functions.
 
 """
+from .lambda_functions import (
+    filter_by_applying_function_to_elements,  # noqa: F401
+    get_list_of_fields_from_a_list_dict,  # noqa: F401
+    sort_a_list_of_dict_by_a_field,  # noqa: F401
+)
