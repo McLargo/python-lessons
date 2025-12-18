a5d46a14932a30f8ea3e1f66a1cd302a00cd8062
Javier Gil <javiergilmont@gmail.com>
2025-12-18 21:24:38 +0100

    feat: Logging with filter

---
+++ src/intermediate/logging/filtering.py
@@ -0,0 +1,50 @@
+"""Custom logging filter to mask sensitive information in log records."""
+
+import logging
+import re
+from logging import LogRecord
+
+
+class CustomFilter(logging.Filter):
+    """Custom logging filter to mask sensitive information in log records."""
+
+    keys_to_mask = {
+        "email": {
+            "pattern": r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+            "mask_func": "mask_email",
+        },
+        "password": {
+            "pattern": r".+",
+            "mask_func": "mask_password",
+        },
+    }
+
+    def filter(self, record: LogRecord) -> bool:
+        """Override filter method to mask sensitive information.
+
+        You can mask sensitive data in record attributes.
+        Also, you can mask record msg or arguments,
+        but only by regex/exact match..
+        """
+        for key in record.__dict__.keys():
+            if key in self.keys_to_mask:
+                # import ipdb; ipdb.set_trace()
+                if isinstance(record.__dict__[key], str):
+                    config = self.keys_to_mask[key]
+                    mask_func = getattr(self, config["mask_func"])
+                    record.__dict__[key] = re.sub(
+                        config["pattern"],
+                        mask_func,
+                        record.__dict__[key],
+                    )
+        return True
+
+    def mask_password(self, match_obj):
+        """Mask password completely."""
+        return "*" * len(match_obj.group(0))
+
+    def mask_email(self, match_obj):
+        """Mask email address, showing only first character before @."""
+        local_part, domain = match_obj.group(0).split("@")
+        masked_local = "*" * (len(local_part))
+        return f"{masked_local}@{domain}"
---
+++ tests/intermediate/logging/test_filtering.py
@@ -0,0 +1,35 @@
+import logging
+
+from src.intermediate.logging import CustomFilter
+
+
+def test_filter_logging(caplog):
+    # add CustomFilter to logger
+    logger = logging.getLogger("src.intermediate.logging.logging")
+    logger.addFilter(CustomFilter())
+
+    # log with sensitive information in extra gets masked
+    data_user = {
+        "username": "user1",
+        "email": "user@example.com",
+        "password": "secret_password",
+        "tries": 3,
+    }
+    logger.info("User logger.", extra=data_user)
+
+    assert len(caplog.records) == 1
+    assert caplog.records[0].message == "User logger."
+    assert caplog.records[0].username == "user1"
+    assert caplog.records[0].email == "****@example.com"
+    assert caplog.records[0].password == "*******************"
+    assert caplog.records[0].tries == 3
+
+    # log with sensitive information as args does not get masked
+    logger.info(
+        "Email %s and password %s",
+        "user@example.com",
+        "Supersecret_password",
+    )
+    assert len(caplog.records) == 2
+    expected_msg = "Email user@example.com and password Supersecret_password"
+    assert caplog.records[1].getMessage() == expected_msg
